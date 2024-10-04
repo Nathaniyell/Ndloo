07995a2f4fd103cd8c850757678a13beda125563
Nathaniyell <essien.nathan@yahoo.com>
2024-10-04 20:37:01 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+ <h4 class="bg-red-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis in ullam accusantium veritatis, magni nobis alias ipsum, quo totam natus maxime repudiandae exercitationem doloribus a, dolorem veniam excepturi quidem voluptatem.</h4>
+</template>
+
+<style scoped>
+
+</style>
