20144981276847ed0ff3828d7f4eb2d3f2362d34
Nathaniyell <essien.nathan@yahoo.com>
2024-10-04 23:24:59 +0100

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,30 @@
 <script setup>
 import logo from "../assets/images/ndloo-logo.png";
 import { RouterLink } from 'vue-router';
+import RouteButton from "../components/RouteButton.vue"
 </script>
 
 <template>
-  <nav class="z-10 absolute top-8 left-0 right-0 bg-dark flex items-center justify-between md:w-11/12 mx-auto">
+  <nav class="z-10 absolute top-8 left-0 right-0 flex items-center justify-between md:w-11/12 mx-auto">
     <RouterLink to="/">
-      <img :src="logo" class="w-1/2" alt="Company logo" />
+      <img :src="logo" class="w-3/4" alt="Company logo" />
     </RouterLink>
     <aside class="flex space-x-4">
-      <RouterLink to="/sign-up" class="bg-white rounded-md text-primary3 px-4 py-2" aria-label="Sign up">
-        Sign up
-      </RouterLink>
-      <RouterLink to="/login" class="bg-transparent border text-white border-white rounded-md px-4 py-2" aria-label="Login">
-        Login
-      </RouterLink>
+             <RouteButton
+    text="Sign up"
+    textColor="text-primary3"
+    bgColor="bg-white"
+    borderColor="border-none"
+    linkTo="/sign-up"
+  />
+      <RouteButton
+    text="Login"
+    textColor="text-white"
+    bgColor="bg-transparent"
+    borderColor="border-white"
+    linkTo="/login"
+  />
+   
     </aside>
   </nav>
 </template>
