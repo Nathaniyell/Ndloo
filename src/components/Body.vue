438979e070f3bb8a71fe1a284655fb0e2156ab59
Nathaniyell <essien.nathan@yahoo.com>
2024-10-06 20:55:29 +0100

    delete 2 files and update 1 file

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -3,7 +3,14 @@ import feature1 from "@/assets/images/feature1.png"
 import feature2 from "@/assets/images/feature2.png"
 import feature3 from "@/assets/images/feature3.png"
 import sectionImg from "@/assets/images/hero2.png"
+import support from "@/assets/icons/24-support.png"
+import security from "@/assets/icons/security-safe.png"
+import bitcoin from "@/assets/icons/bitcoin-card.png"
+import profile from "@/assets/icons/profile-tick.png"
+import phone from "@/assets/icons/phone.png"
+import report from "@/assets/icons/report.png"
 import Card from "./Card.vue"
+import ServiceCard from "./ServiceCard.vue"
 
 const featuresData = [
     {
@@ -19,6 +26,32 @@ const featuresData = [
         title: "Easy communication and best user interface"
     },
 ]
+const serviceData = [
+    {
+        image: support,
+        title: "24/7 Support Service"
+    },
+    {
+        image: security,
+        title: "Secured Connection"
+    },
+    {
+        image: phone,
+        title: "Easy user interface"
+    },
+    {
+        image: bitcoin,
+        title: "Crypto payment method"
+    },
+    {
+        image: profile,
+        title: "Custom profile fields"
+    },
+    {
+        image: report,
+        title: "Abuse report facility"
+    },
+]
 </script>
 
 <template>
@@ -28,19 +61,30 @@ const featuresData = [
             <span class="text-primary2">Ndloo</span> today
         </h3>
         <div class="grid md:grid-cols-3 gap-8 md:place-items-center md:items-stretch">
-            <Card v-for="(data, index) in featuresData" :key="index" :title="data.title" :image="data.image" />
+            <Card v-for="(feature, index) in featuresData" :key="index" :title="feature.title" :image="feature.image" />
         </div>
     </div>
-    <div
-        class="text-white bg-primary3 mb-16 bg-blend-overlay bg-no-repeat grid place-items-center bg-cover bg-center md:h-screen"
-        :style="{ backgroundImage: `url(${sectionImg})` }"
-    >
+    <div class="text-white bg-primary3 mb-16 bg-blend-overlay bg-no-repeat grid place-items-center bg-cover bg-center md:h-screen"
+        :style="{ backgroundImage: `url(${sectionImg})` }">
         <div class="w-11/12 lg:w-[85%] mx-auto flex flex-col justify-center items-center gap-16 py-20 text-center">
             <h5 class="text-5xl">What our users say</h5>
             <p class="text-2xl">
-                Other dating apps were like shooting fish in a barrel. But my girlfriend and I clicked right away on Ndloo, and the conversation was effortless. We’ve been together for over a year.
+                Other dating apps were like shooting fish in a barrel. But my girlfriend and I clicked right away on
+                Ndloo, and the conversation was effortless. We’ve been together for over a year.
             </p>
             <p class="text-5xl">V. Peter</p>
         </div>
     </div>
+    <div class="bg-light">
+        <div class="w-11/12 lg:w-[85%] mx-auto py-20 leading-8">
+            <h3 class="text-4xl mb-10">
+                Why use<span class="text-primary2 ml-1">Ndloo</span> to find <br> your perfect match?
+            </h3>
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:place-items-center md:items-stretch">
+                <ServiceCard v-for="(service, index) in serviceData" :key="index" :title="service.title"
+                    :icon="service.image" />
+            </div>
+        </div>
+    </div>
+
 </template>
---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+
+
+const props = defineProps({
+    icon: {
+    type: [String, Object],
+    required: true,
+  },
+  title:{
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+<div class="flex flex-col space-y-6 items-center justify-center p-8 bg-white rounded-md shadow">
+    <img :src="icon" alt="title" class="w-1/3 mx-auto">
+    <h6 class="text-xl font-[550] text-center">{{ title }}</h6>
+</div>
+</template>
