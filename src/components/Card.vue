112b6925fe3685102f8d4139bbfd746d33b8d3b4
Nathaniyell <essien.nathan@yahoo.com>
2024-10-06 19:34:42 +0100

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+
+
+const props = defineProps({
+    image: {
+    type: [String, Object],
+    required: true,
+  },
+  title:{
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+
+</template>
