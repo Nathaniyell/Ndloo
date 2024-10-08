93100341bf8838e09a971462cf699657787a9c64
Nathaniyell <essien.nathan@yahoo.com>
2024-10-08 16:53:56 +0100

    update SignUpView.vue

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -4,11 +4,11 @@ import logo from "@/assets/images/ndloo.png";
 import loginBg from "@/assets/images/loginBg.png"
 import { RouterLink } from 'vue-router';
 
-const loginFormData = ref({
+const formData = ref({
     firstName: "",
     lastName: "",
     username: "",
-    mobileNo: "",
+    mobileNo: { phoneNumber: "", countryCode: "" },
     gender: "",
     dateOfBirth: "",
     email: "",
@@ -16,39 +16,60 @@ const loginFormData = ref({
     password2: "",
     termsAndConditions: false
 })
+const currentStep = ref(1);
+const totalSteps = 2;
 
-const loginFormSubmitHandler = () => {
+const nextStep = () => {
+  if (currentStep.value < totalSteps) {
+    currentStep.value += 1;
+  }
+};
+
+const prevStep = () => {
+  if (currentStep.value > 1) {
+    currentStep.value -= 1;
+  }
+};
+
+const signUpFormSubmitHandler = () => {
+ 
     // Log the current values before resetting
     console.log({
-        email: loginFormData.value.firstName,
-        password: loginFormData.value.lastName,
-        password: loginFormData.value.username,
-        password: loginFormData.value.mobileNo,
-        password: loginFormData.value.gender,
-        password: loginFormData.value.dateOfBirth,
-        password: loginFormData.value.email,
-        password: loginFormData.value.password1,
-        password: loginFormData.value.password2,
-        rememberMe: loginFormData.value.termsAndConditions
+        email: formData.value.firstName,
+        password: formData.value.lastName,
+        password: formData.value.username,
+        password: formData.value.mobileNo,
+        password: formData.value.gender,
+        password: formData.value.dateOfBirth,
+        password: formData.value.email,
+        password: formData.value.password1,
+        password: formData.value.password2,
+        rememberMe: formData.value.termsAndConditions
     });
-
-    // Reset the form data
-    loginFormData.value.firstName = ""
-    loginFormData.value.lastName = ""
-    loginFormData.value.username = ""
-    loginFormData.value.mobileNo = ""
-    loginFormData.value.gender = ""
-    loginFormData.value.dateOfBirth = ""
-    loginFormData.value.email = ""
-    loginFormData.value.password1 = ""
-    loginFormData.value.password2 = ""
-    loginFormData.value.termsAndConditions = ""
-
+formData.value = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    mobileNo: { phoneNumber: "", countryCode: "" },
+    gender: "",
+    dateOfBirth: "",
+    email: "",
+    password1: "",
+    password2: "",
+    termsAndConditions: false
 }
-// Toggle visibility of the password
-const isPasswordVisible = ref(false)
-const togglePasswordVisibility = () => {
-    isPasswordVisible.value = !isPasswordVisible.value
+    // Reset the form data
+    // formData.value.firstName = ""
+    // formData.value.lastName = ""
+    // formData.value.username = ""
+    // formData.value.mobileNo = ""
+    // formData.value.gender = ""
+    // formData.value.dateOfBirth = ""
+    // formData.value.email = ""
+    // formData.value.password1 = ""
+    // formData.value.password2 = ""
+    // formData.value.termsAndConditions = ""
+    currentStep.value = 1;
 }
 </script>
 
@@ -59,68 +80,70 @@ const togglePasswordVisibility = () => {
             :style="{ backgroundImage: `url(${loginBg})` }">
             <img :src="logo" alt="logo" />
         </div>
-        <form class="h-screen bg-white md:w-1/2 py-4 md:py-2" @submit.prevent="loginFormSubmitHandler">
-            <div class="w-11/12 lg:w-[75%] mx-auto flex flex-col py-6 lg:py-4 gap-14 lg:gap-12">
+        <form class="h-screen bg-white md:w-1/2 py-4 md:py-2" @submit.prevent="signUpFormSubmitHandler">
+            <div v-if="currentStep === 1" class="w-11/12 lg:w-[75%] mx-auto flex flex-col py-6 lg:py-4 gap-12 lg:gap-10">
 
 
                 <div class="flex flex-col space-y-2 text-center">
                     <h1 class="text-4xl font-semibold">Create an account </h1>
                     <p class="text-[#6A6A6A]">Let the love journey begin now</p>
                 </div>
-                <div class="grid space-y-8 md:space-y-5">
-                    <div class="grid space-y-4 md:space-y-0 md:space-x-6 md:grid-cols-2">
+                <div class="grid space-y-6 md:space-y-4">
+                    <div class="flex flex-col space-y-4 md:space-y-0 md:items-center md:space-x-6 md:flex-row">
 
-                        <input v-model="loginFormData.firstName" required type="firstName" placeholder="First Name"
-                            class="text-[#6A6A6A] font-semibold bg-light bg-opacity-20 w-full p-3  pr-10 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
-                        <input v-model="loginFormData.firstName" required type="firstName" placeholder="First Name"
-                            class="text-[#6A6A6A] font-semibold bg-light bg-opacity-20 w-full p-3  pr-10 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
+                        <input v-model="formData.firstName" required type="firstName" placeholder="First name"
+                            class="text-[#6A6A6A] font-semibold bg-light bg-opacity-20 w-full p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
+                        <input v-model="formData.lastName" required type="lastName" placeholder="Last name"
+                            class="text-[#6A6A6A] font-semibold bg-light bg-opacity-20 w-full p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
 
                     </div>
                     <div class="relative">
-                        <input v-model="loginFormData.password" :type="isPasswordVisible ? 'text' : 'password'"
-                            placeholder="Password" required
-                            class="text-[#6A6A6A] font-semibold bg-light bg-opacity-20 w-full p-3 pr-10 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
-                        <button @click="togglePasswordVisibility" type="button"
-                            class="pointer-events-none  absolute right-3 top-1/2 transform -translate-y-1/2 focus:outline-none">
-                            <font-awesome-icon v-if="isPasswordVisible" icon="fa-regular fa-eye-slash"
-                                class="text-[#E68D8D]" />
-                            <font-awesome-icon v-else icon="fa-regular fa-eye" class="text-[#E68D8D]" />
-                        </button>
-
-                        <div class="flex w-full justify-between items-center mt-4 absolute">
-                            <div class="flex items-center space-x-3">
-
-                                <input class="checked:!bg-primary4" v-model="loginFormData.rememberMe" type="checkbox"
-                                    id="rememberMe">
-                                <label for="rememberMe" class="text-[#6A6A6A] font-semibold text-sm">Remember me</label>
-                            </div>
-                            <RouterLink to="/forgot-password" class="text-primary3 font-semibold text-sm">Forgot
-                                password?</RouterLink>
-                        </div>
+                        <input v-model="formData.username" type="text" placeholder="Username" required
+                            class="text-[#6A6A6A] font-semibold bg-light bg-opacity-20 w-full p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
                     </div>
+                    <div class="flex items-center space-x-2 w-full">
+                        <!-- Country Code Select -->
+                        <select v-model="formData.mobileNo.countryCode"
+                            class="w-[30%] text-[#6A6A6A] font-semibold bg-light bg-opacity-20 p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded">
+                            <option value="">+234 </option>
+                            <option value="+1">+234 (NGA)</option>
+                            <option value="+1">+1 (US)</option>
+                            <option value="+44">+44 (UK)</option>
+                            <option value="+91">+91 (IN)</option>
+                            <option value="+234">+234 (NG)</option>
+                            <option value="+61">+61 (AU)</option>
+                            <option value="+81">+81 (JP)</option>
+                            <!-- Add more country codes as needed -->
+                        </select>
+
+                        <input type="text" v-model="formData.mobileNo.phoneNumber" placeholder="Mobile Number"
+                            class="w-[70%] text-[#6A6A6A] font-semibold bg-light bg-opacity-20 p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
+                    </div>
+
+                    <div class="w-full">
+                        <select v-model="formData.gender"
+                            class="w-full text-[#6A6A6A] font-semibold bg-light bg-opacity-20 p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded">
+                            <option value="">Select gender </option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <div class="w-full">
+                        <input type="date" v-model="formData.dateOfBirth"
+                            class="w-full text-[#6A6A6A] font-semibold bg-light bg-opacity-20 p-3 border border-[#C9C9C9] outline-none focus:!border-primary3 active:border-primary3 rounded" />
+
+                    </div>
+
                 </div>
-                <section class="flex flex-col space-y-5 items-center text-[#6A6A6A]">
-                    <div class="flex gap-3 items-center w-full">
-                        <span class="flex-1 bg-[#656565] h-[0.1rem]"></span>
-                        or
-                        <span class="flex-1 bg-[#656565] h-[0.1rem]"></span>
-                    </div>
-
-
-                    <RouterLink
-                        class="bg-transparent text-[#656565] p-3 font-semibold !border w-full text-center grid place-items-center rounded border-[#C9C9C9]"
-                        to="/otp-login">
-                        Login with OTP
-                    </RouterLink>
-                </section>
                 <section class="flex flex-col space-y-3 items-center text-[#6A6A6A]">
 
-                    <button type="submit"
+                    <button @click="nextStep" type="button"
                         class="bg-primary3 text-white p-3 font-semibold w-full text-center grid place-items-center rounded ">
-                        Login
+                        Next
                     </button>
-                    <label class="text-sm">Don't have an account? <RouterLink to="/signup"
-                            class="text-primary3 font-semibold text-base">Sign up</RouterLink></label>
+                    <label class="text-sm">Already have an account? <RouterLink to="/login"
+                            class="text-primary3 font-semibold text-base">Login</RouterLink></label>
                 </section>
             </div>
         </form>
