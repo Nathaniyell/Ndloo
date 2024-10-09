d301d821f676c93067baf588688800f376fac0d1
Nathaniyell <essien.nathan@yahoo.com>
2024-10-09 22:31:18 +0100

    update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,33 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
 import logo from "@/assets/images/ndloo-logo.png";
 import { RouterLink } from 'vue-router';
-import RouteButton from "./RouteButton.vue"
+import RouteButton from "./RouteButton.vue";
+
+
+const isScrolled = ref(false);
+
+// Add a scroll event listener to update the isScrolled state
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 70;
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
 </script>
 
 <template>
-  <nav class="z-10 bg-transparent absolute top-10 left-0 right-0 flex items-center justify-between w-11/12 lg:w-[85%] mx-auto">
+  <nav
+    :class="[
+      'z-10 fixed p-3 md:py-3 md:px-5 transition-all ease-in-out duration-200 left-0 right-0 flex items-center justify-between w-11/12 lg:w-[90%] mx-auto',
+      isScrolled ? 'bg-primary3' : 'bg-transparent'
+    ]"
+  >
     <RouterLink to="/">
       <img :src="logo" class="w-3/5 md:w-3/4" alt="Company logo" />
     </RouterLink>
@@ -16,7 +38,6 @@ import RouteButton from "./RouteButton.vue"
       <RouteButton text="Login" bgColor="bg-transparent" borderColor="border-light border-2" linkTo="login"
         ariaLabel="Login" hoverTextColor="hover:text-primary3" hoverBgColor="hover:bg-light"
         hoverBorderColor="hover:border-light" class="" />
-
     </aside>
   </nav>
 </template>
