9633a03ff72b3b09de2b4e534204d10ded215c00
Nathaniyell <essien.nathan@yahoo.com>
2024-10-18 15:05:48 +0100

    update Sidebar.vue

--- src/components/dashboard/Sidebar.vue
+++ src/components/dashboard/Sidebar.vue
@@ -3,7 +3,7 @@ import NavItem from "./NavItem.vue";
 import MoreItems from "./MoreItems.vue";
 import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
-import { moreIcon, settingsIcon, logoutIcon, navItems, moreItems, asideImg } from "@/data/data"; // Import relevant assets
+import {logo, moreIcon, settingsIcon, logoutIcon, navItems, moreItems, asideImg } from "@/data/data"; // Import relevant assets
 
 const showMore = ref(false);
 const setShowMore = () => showMore.value = !showMore.value;
@@ -21,7 +21,7 @@ const logout = () => {
         class="w-full z-10 mt-20 md:mt-0 md:w-1/4 fixed bottom-0 md:bottom-auto bg-[#8500288A] bg-blend-overlay bg-opacity-80 bg-no-repeat bg-center bg-cover p-2 md:p-4 flex flex-row md:flex-col justify-center md:justify-start items-center md:items-start md:space-y-6"
         :style="{ backgroundImage: `url(${asideImg})` }">
         <RouterLink to="/">
-            <img class="md:w-3/5 hidden md:block mb-4" alt="Ndloo Logo" />
+            <img :src="logo" class="md:w-3/5 hidden md:block mb-4" alt="Ndloo Logo" />
         </RouterLink>
 
         <nav class="flex justify-between flex-row md:flex-col w-full md:space-y-3 md:justify-start">
