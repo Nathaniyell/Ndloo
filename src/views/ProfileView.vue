3b7613b5a423c1df9c8c68f6fd9c79fa4b3ab5c4
Nathaniyell <essien.nathan@yahoo.com>
2024-10-24 17:45:15 +0100

    update user Profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AsideProfile from '@/components/AsideProfile.vue';
+import UserProfile from '@/components/profile/UserProfile.vue';
 
 
 </script>
 
 <template>
-  <div class="grid md:grid-cols-[1fr_2fr]">
+  <div class="grid md:grid-cols-[1fr_2fr] px-4 pb-6 gap-4">
 <AsideProfile />
+<UserProfile />
   </div>
 </template>
